448151196ef9698768dd72bec96d64653286bae2
Rajiv Sah <rajiv.shah01234@gmail.com>
2019-01-26 10:42:21 +0545

    find temprature

---
+++ app/assets/javascripts/weathers.js
@@ -0,0 +1,15 @@
+// window.onload = function() {
+//         var location_form = document.getElementById('location-form');
+//         location_form.addEventListener('submit', function(event) {
+//             event.preventDefault();
+//             console.log(event)
+//             var location;
+//             $.ajax({
+//                 method: 'GET',
+//                 url: url
+//             }).done(function(result){
+//                 console.log(result)
+//             })
+
+//         })
+//     }
--- app/controllers/weathers_controller.rb
+++ app/controllers/weathers_controller.rb
@@ -1,4 +1,26 @@
 class WeathersController < ApplicationController
+  protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token  
+
   def index
+    if params[:location]
+      url1 = 'http://api.openweathermap.org/data/2.5'\
+            "/weather?q=#{params[:location]}"\
+            '&appid=f261d4b9a214e0ea3c3ae99e6c357fb0&units=metric'
+      url2 = 'http://api.apixu.com/v1/current.json?'\
+              "key=7f0582ae268941a1b2f41827192601&q=#{params[:location]}"
+      begin
+        temprature1 = get_temperature(url1)['main']['temp']
+        temprature2 = get_temperature(url2)['current']['temp_c']
+        @temprature = (temprature1 + temprature2) / 2
+      rescue RestClient::NotFound
+        @error = 'Please enter a valid location'
+      end
+    end
+  end
+
+  def get_temperature(url)
+    response = RestClient.get url
+    JSON.parse response.body
   end
 end
